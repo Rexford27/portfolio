8450eacf6823526459cdba441302d50c06e2cb93
Rexford27 <rexfordfrempong7@gmail.com>
2024-08-04 12:52:10 -0400

    aug 4 responsive test_1

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
-window.addEventListener("resize", check_screen_size )
+
+/*window.addEventListener("resize", check_screen_size )
 
 var enable_mobile_display = false
+
+var catalog = document.getElementById("catalog")
+var catalog_btns = document.getElementsByClassName("catalog_btn")
+
 function check_screen_size(){
-  if (window.innerWidth < 700){
+  console.log(window.innerWidth)
+  if (window.innerWidth < 800){
     enable_mobile_display = true
+    catalog.classList.add("catalog_mobile")
     
   }
   else{
     enable_mobile_display = false
+    catalog.classList.remove("catalog_mobile")
   }
   console.log(enable_mobile_display)
 }
+  */
